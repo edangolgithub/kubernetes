a24bc191c90a4c4fb0e6bd39150c548301320fa5
edangolgithub <dangolevan@gmail.com>
2021-09-22 20:51:30 +0545

    first commit

---
+++ simplephp/phpsrc/index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Inside K8s with MySQL <br>";
+$conn = new mysqli("mysql", "root", "password", "school");
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name FROM student";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo $row['name']."<br>";
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
